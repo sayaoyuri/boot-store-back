e0a49b60ec9ce2acbecba5929f29eda7155f123c
sayaoyuri <yuriantunes0000@gmail.com>
2023-07-15 10:21:24 -0300

    Feat: Adding /games endpoint to list all registered games

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -10,3 +10,13 @@ export const registerGame = async (req, res) => {
     return res.status(500).send(e.message);
   }
 }
+
+export const getAllGames = async (req, res) => {
+  try {
+    const games = await db.collection('games').find().limit(100).toArray();
+    console.log(games);
+    return res.send(games);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  };
+};
