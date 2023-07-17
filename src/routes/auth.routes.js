f4ef86f39138e6ed9a381b085d9642ae576794f0
José Matheus <XxFULLDLCxX@outlook.com>
2023-07-17 14:37:35 -0300

    Fix: Remoção completa do componente Account

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAccount, signin, signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/schema.validate.js";
 import { signin_schema, signup_schema } from "../schemas/auth.schemas.js";
 import { getDate } from "../middlewares/date.utils.js";
@@ -8,5 +8,4 @@ const auth_Router = Router();
 auth_Router.use(getDate);
 auth_Router.post("/sign-up", validateSchema(signup_schema), signup);
 auth_Router.post("/sign-in", validateSchema(signin_schema), signin);
-auth_Router.get("/account", getAccount);
 export default auth_Router;
