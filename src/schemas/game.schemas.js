0e75c4ef341952de2efd0385546fd28e3d4e2fc5
José Matheus <XxFULLDLCxX@outlook.com>
2023-07-17 13:09:38 -0300

    Chore: Aumentando um pouco o limite de caracteres para o name do game

--- src/schemas/game.schemas.js
+++ src/schemas/game.schemas.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const registerGameSchema = Joi.object({
-  name: Joi.string().min(4).max(40).required(),
+  name: Joi.string().min(4).max(50).required(),
   category: Joi.string().required(),
   description: Joi.string().min(4).max(200).required(),
   price: Joi.number().min(0).required(),
--- src/schemas/orders.schema.js
+++ src/schemas/orders.schema.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const order_schema = Joi.object({
-  name: Joi.string().min(4).max(40).required(),
+  name: Joi.string().min(4).max(50).required(),
   category: Joi.string().required(),
   price: Joi.number().min(0).required(),
   platform: Joi.string().required(),
